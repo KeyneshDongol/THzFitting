c036f034799eb995050f253dd9f0161d0903fcc9
YingshuYang <yingshu001@e.ntu.edu.sg>
2023-10-25 12:14:38 +0800

    renamed and changed details

--- Main_to_run.py
+++ Main_to_run.py
@@ -6,9 +6,9 @@ from functools import partial
 import json
 import time
 
-from src.Read_material import Material
-from src.TMM import SpecialMatrix
-from src import exp_pulse, fourier, tools
+from Functions.Read_material import Material
+from Functions.TMM import SpecialMatrix
+from Functions import exp_pulse, fourier, tools
 
 
 def E_TMM(layers, to_find, omega, eps0, mu, d, f_in,sub_layer, echoes_removed, unknown):  # returns the theoretically transmitted pulse (in both time and frequency domain)
@@ -68,7 +68,7 @@ def Error_func(layers, to_find, omega, eps0, mu, d, E_air_f, E_exp_f,sub_layer,e
 if __name__ == '__main__':
     ### drag in all information defined in config json file
     # f = open('/Users/yingshuyang/pythonfiles/TransferMatrixMethod/A1_THz_nk_Fitting/config.json')
-    f = open(Path.cwd()/'config.json')
+    f = open(Path.cwd()/'inputs.json')
     config = json.load(f)
     to_find = list(config['to_find'].values()) 
     input_num = list(config['input'].values())
